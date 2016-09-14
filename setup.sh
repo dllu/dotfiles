cf1d24c63ad29fb1f84ff36f18e82b7ee8297c1b
Daniel L. Lu <daniel.lu@ouster.io>
2016-09-14 14:11:10 -0700

    update setup.sh with qutebrowser.conf

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-mkdir -p ~/.config/nvim ~/.config/i3 ~/.config/fish
+mkdir -p ~/.config/nvim ~/.config/i3 ~/.config/fish ~/.config/qutebrowser
 curdir=$(pwd)
 ln -s $curdir/nvimrc ~/.config/nvim/init.vim
 ln -s $curdir/i3config ~/.config/i3/config
 ln -s $curdir/config.fish ~/.config/fish/config.fish
 ln -s $curdir/Xresources ~/.config/Xresources
+ln -s $curdir/qutebrowser.conf ~/.config/qutebrowser/qutebrowser.conf
 
