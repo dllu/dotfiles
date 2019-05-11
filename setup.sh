c8e61ed5cd64b89f7878e5d9915c048fd6078301
Daniel Lu <daniel@lawrence.lu>
2019-05-10 23:04:41 -0700

    add fonts.conf

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mkdir -p ~/.config/nvim ~/.config/sway ~/.config/fish
+mkdir -p ~/.config/nvim ~/.config/sway ~/.config/fish ~/.config/fontconfig
 curdir=$(pwd)
 ln -s $curdir/nvimrc ~/.config/nvim/init.vim
 ln -s $curdir/i3config ~/.config/sway/config
 ln -s $curdir/config.fish ~/.config/fish/config.fish
 ln -s $curdir/Xresources ~/.config/Xresources
+ln -s $curdir/fonts.conf ~/.config/fontconfig/fonts.conf
