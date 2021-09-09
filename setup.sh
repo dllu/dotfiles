77f99f7843cb49e663bb58066e8d8b5f94966d9d
Daniel L. Lu <daniel@lawrence.lu>
2021-09-09 11:47:15 -0700

    neofetch

--- setup.sh
+++ setup.sh
@@ -8,3 +8,5 @@ ln -s $curdir/Xresources ~/.config/Xresources
 ln -s $curdir/fonts.conf ~/.config/fontconfig/fonts.conf
 ln -s $curdir/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s $curdir/rofi.rasi ~/.config/rofi/dllu.rasi
+ln -s $curdir/neofetch.conf ~/.config/neofetch/config.conf
+ln -s $curdir/pup.txt ~/.config/neofetch/pup.txt
