95e8645c12bff5076277489958b4e54d4af8fc16
Daniel L. Lu <daniel@lawrence.lu>
2022-06-11 12:47:07 -0700

    add sway and dllu.vim

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.config/nvim ~/.config/sway ~/.config/fish ~/.config/fontconfig
 curdir=$(pwd)
 ln -s $curdir/nvimrc ~/.config/nvim/init.vim
 ln -s $curdir/i3config ~/.config/i3/config
+ln -s $curdir/swayconfig ~/.config/sway/config
 ln -s $curdir/config.fish ~/.config/fish/config.fish
 ln -s $curdir/Xresources ~/.config/Xresources
 ln -s $curdir/fonts.conf ~/.config/fontconfig/fonts.conf
@@ -10,3 +11,4 @@ ln -s $curdir/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s $curdir/rofi.rasi ~/.config/rofi/dllu.rasi
 ln -s $curdir/neofetch.conf ~/.config/neofetch/config.conf
 ln -s $curdir/pup.txt ~/.config/neofetch/pup.txt
+ln -s $curdir/dllu.vim ~/.config/nvim/colors/dllu.vim
